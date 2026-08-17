e2c4de8411f29219130e0a8f9ccc7ac3234b2269
netinhobagaso-maker <netinhobagaso@gmail.com>
2026-08-17 16:24:25 -0300

    Update page.js

--- src/app/vender/page.js
+++ src/app/vender/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { supabase } from '@/lib/supabase';
-import BottomNav from '@/components/BottomNav';
+import { supabase } from '../../lib/supabase';
+import BottomNav from '../../components/BottomNav';
 
 export default function Vender() {
   const [produtos, setProdutos] = useState([]);
